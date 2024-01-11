793e38dc744b6e52aabde01b99959280824ca855
bongsikhan <bongsikhan@gmail.com>
2024-01-12 02:43:48 +0900

    ✔ 쇼케이스 - 여백 추가

--- www/pages/myroom.php
+++ www/pages/myroom.php
@@ -200,7 +200,7 @@
                                 <li class="active"><a href="#tab-showcase" data-target="#showcase"><div class="tab-name">쇼케이스</div></a></li>
                                 <li><a href="#rolling-paper" data-target="#rolling-paper"><div class="tab-name">롤링페이퍼</div></a></li>
 <!--                                <li><a href="#energizer" data-target="#energizer"><div class="tab-name">에너자이저</div></a></li>-->
-                                <li><a href="#charger" data-target="#charger"><div class="tab-name"><img class="ico-charger" src="/assets/img/ico/ico-charger.png" alt="charger">차저</div></a></li>
+                                <li><a href="#charger" data-target="#charger"><div class="tab-name"><!--<img class="ico-charger" src="/assets/img/ico/ico-charger.png" alt="charger">-->⚡차저</div></a></li>
                             </ul>
                             <ul class="big">
                                 <li><a href="#received" data-target="#received"><div class="tab-name">받은 상장</div><div class="num">12</div></a></li>
